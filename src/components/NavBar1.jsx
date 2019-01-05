26d6c65ababcfe06a1f84dd5dec9026891c9b5a3
Justin Gomez <programjustin@gmail.com>
2019-01-05 10:51:55 -0800

    position links

--- src/components/NavBar1.jsx
+++ src/components/NavBar1.jsx
@@ -13,16 +13,18 @@ function NavBar1() {
                 flex-direction: row;
                 height: 40px;
                 width: 100%;
-                padding-right: 60px;
               }
               a {
                 margin-left: 20px;
+              }
+              #cart {
+                margin-right: 20px;
               }
             `}
       </style>
       <a>Login/Join</a>
       <a>Help</a>
-      <a>cart</a>
+      <a id="cart">cart</a>
     </div>
   );
 }
