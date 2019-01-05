b1747d1d671845ddbca2c0573d46ea8aaafadb08
Justin Gomez <programjustin@gmail.com>
2019-01-05 12:10:39 -0800

    add a Hero Component

---
+++ src/components/HeroLanding1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HeroLanding1() {
+  return(
+    <div id="heroLanding1">
+      <style global jsx>{`
+            #heroLanding1 {
+              height: 525px;
+              background-color: grey;
+            }
+        `}</style>
+    </div>
+  );
+}
+
+export default HeroLanding1;
