88f6fb8a2b637ea45f52f55725cb5811b0d05163
Justin Gomez <programjustin@gmail.com>
2019-01-04 19:46:43 -0800

    Add a Navigation component

---
+++ src/components/NavBar1.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function NavBar1() {
+  return(
+    <div id="navBar1">
+    <style jsx global>{`
+            #navBar1 {
+              display: flex;
+              border: 1px solid black;
+              flex-direction: row;
+              height: 40px;
+              width: 100%;
+            }
+          `}
+    </style>
+    </div>
+  );
+}
+
+export default NavBar1;
