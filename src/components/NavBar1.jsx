0cab9272e01f11535036efc26802f49e7f0be2f9
Justin Gomez <programjustin@gmail.com>
2019-01-04 20:57:58 -0800

    add links

--- src/components/NavBar1.jsx
+++ src/components/NavBar1.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 function NavBar1() {
   return(
     <div id="navBar1">
-    <style jsx global>{`
-            #navBar1 {
-              display: flex;
-              border: 1px solid black;
-              flex-direction: row;
-              height: 40px;
-              width: 100%;
-            }
-          `}
-    </style>
+      <style jsx global>{`
+              #navBar1 {
+                display: flex;
+                flex-direction: column;
+                border: 1px solid black;
+                flex-direction: row;
+                height: 40px;
+                width: 100%;
+              }
+            `}
+      </style>
+      <a>Login/Join</a>
+      <a>Help</a>
+      <a>cart</a>
+
     </div>
   );
 }
