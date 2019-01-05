ee82b94d1e6be3adb1929cd7b683ffb24aa625b3
Justin Gomez <programjustin@gmail.com>
2019-01-05 12:36:06 -0800

    add a landing page component

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import HeroLanding1 from './HeroLanding1';
+
+class LandingPage extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return(
+      <div>
+        <style global jsx>{`
+          
+          `}</style>
+        <HeroLanding1 />
+      </div>
+    );
+  }
+
+
+}
+
+export default LandingPage;
