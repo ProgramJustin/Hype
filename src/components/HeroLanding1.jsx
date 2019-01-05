a27abb09a3f7e6af3cbb3c64b8864d85d0815314
Justin Gomez <programjustin@gmail.com>
2019-01-05 13:05:25 -0800

    position content

--- src/components/HeroLanding1.jsx
+++ src/components/HeroLanding1.jsx
@@ -7,6 +7,13 @@ function HeroLanding1() {
             #heroLanding1 {
               height: 525px;
               background-color: grey;
+              display: flex;
+              flex-direction: row;
+              justify-content: flex-end;
+              align-items: center;
+            }
+            #contentContainerHero1 {
+              margin-right: 60px;
             }
             #airJordanButton {
               width: 75px;
@@ -15,7 +22,7 @@ function HeroLanding1() {
               background-color: white;
             }
         `}</style>
-        <div>
+        <div id="contentContainerHero1">
           <h1>Air Jordan 1</h1>
           <div id="airJordanButton">Shop All</div>
         </div>
