e9bec5682a8128d1164a974ae4946af6c9c47adc
Justin Gomez <programjustin@gmail.com>
2019-01-04 21:29:59 -0800

    display links on the right side of the navigation bar

--- src/components/NavBar1.jsx
+++ src/components/NavBar1.jsx
@@ -7,17 +7,22 @@ function NavBar1() {
               #navBar1 {
                 display: flex;
                 flex-direction: column;
+                justify-content: flex-end;
+                align-items: center;
                 border: 1px solid black;
                 flex-direction: row;
                 height: 40px;
                 width: 100%;
+                padding-right: 60px;
+              }
+              a {
+                margin-left: 20px;
               }
             `}
       </style>
       <a>Login/Join</a>
       <a>Help</a>
       <a>cart</a>
-
     </div>
   );
 }
