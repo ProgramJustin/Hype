7bca8ef2044055cf95af4c6660d9788e335d6b99
Justin Gomez <programjustin@gmail.com>
2019-01-05 09:38:23 -0800

    add a navigation bar component

---
+++ src/components/NavBar2.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function NavBar2() {
+
+  return(
+    <div id="NavBar2">
+      <style jsx global>{`
+              #NavBar2 {
+                display: flex;
+                flex-direction: row;
+                width: 100%;
+              }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default NavBar2;
