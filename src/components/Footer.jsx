da6f5dcc13572fc0b67e781e398c9960c58614b6
Justin Gomez <programjustin@gmail.com>
2019-01-05 17:02:41 -0800

    add link content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,42 +5,52 @@ function Footer(){
     <div id="footer">
       <style global jsx>{`
           #footer {
-            height: 241px;
-            border: 1px solid black;
             display: flex;
+            height: 100%;
+            width: 100%;
+            border: 1px solid black;
           }
 
           #footer div {
+            display: flex;
+            flex-direction: row;
             height: 100%;
             width: 100%;
           }
 
           #footer div div {
             display: flex;
-            border: 1px solid black;
+            flex-direction: column;
+            padding: 10px;
+          }
+          #footer div div a {
+            margin-bottom: 15px;
+            font-size: 13px;
+          }
+
+          @media only screen and (max-width: 799.9px) {
+            #footer {
+              flex-direction: column;
+              height: 100%;
+            }
           }
         `}</style>
       <div>
         <div>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
+          <a>Find a Store</a>
+          <a>Site Feedback</a>
         </div>
         <div>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
+          <a>Get Help</a>
+          <a>Shipping & Delivery</a>
+          <a>Returns</a>
+          <a>Payment Options</a>
+          <a>Contact Us</a>
         </div>
         <div>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
-          <a>Data</a>
+          <a>About Hype</a>
+          <a>News</a>
+          <a>Careers</a>
         </div>
       </div>
 
