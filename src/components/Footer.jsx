d853208f072c00f13ad180e4f53e46056dc2d933
Justin Gomez <programjustin@gmail.com>
2019-01-05 16:33:14 -0800

    add a links and containers that will house the links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,38 @@ function Footer(){
           #footer {
             height: 241px;
             border: 1px solid black;
+            display: flex;
           }
         `}</style>
-      Data
+      <div>
+        <div>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+        </div>
+        <div>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+        </div>
+        <div>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+          <a>Data</a>
+        </div>
+      </div>
+
+      <div>
+        <img src="" />
+        <img src="" />
+        <img src="" />
+      </div>
     </div>
   );
 }
