64b575566955cac5e8d4d85a434aceb162c45858
Justin Gomez <programjustin@gmail.com>
2019-01-05 19:01:47 -0800

    convert functional component into a class component

--- src/components/HeroLanding1.jsx
+++ src/components/HeroLanding1.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import AirJordan from '../../public/AirJordanBackground.png'
+import AirJordan from '../../public/AirJordanBackground.png';
 
-function HeroLanding1() {
-  return(
-    <div id="heroLanding1">
-      <style global jsx>{`
-            #heroLanding1 {
-              height: 525px;
-              display: flex;
-              flex-direction: row;
-              justify-content: flex-end;
-              align-items: center;
-            }
-            #contentContainerHero1 {
-              margin-right: 60px;
-            }
-            #airJordanButton {
-              width: 75px;
-              border-radius: 2px;
-              padding: 15px;
-              background-color: white;
-              margin-top: 20px;
-            }
-        `}</style>
-        <div id="contentContainerHero1">
-          <h1>Air Jordan 1</h1>
-          <div id="airJordanButton">Shop All</div>
-        </div>
-    </div>
-  );
+class HeroLanding1 extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      backgroungImage: AirJordan
+    };
+  }
+  render(){
+    return(
+      <div id="heroLanding1">
+        <style global jsx>{`
+              #heroLanding1 {
+                height: 525px;
+                display: flex;
+                flex-direction: row;
+                justify-content: flex-end;
+                align-items: center;
+              }
+              #contentContainerHero1 {
+                margin-right: 60px;
+              }
+              #airJordanButton {
+                width: 75px;
+                border-radius: 2px;
+                padding: 15px;
+                background-color: white;
+                margin-top: 20px;
+              }
+          `}</style>
+          <div id="contentContainerHero1">
+            <h1>Air Jordan 1</h1>
+            <div id="airJordanButton">Shop All</div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default HeroLanding1;
