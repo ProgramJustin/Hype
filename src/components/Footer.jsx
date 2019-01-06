7cea5a9a547e141e1fb4ed53bd0a930014e141ae
Justin Gomez <programjustin@gmail.com>
2019-01-05 16:24:06 -0800

    add a footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer(){
+  return(
+    <div id="footer">
+      <style global jsx>{`
+          #footer {
+            height: 241px;
+            border: 1px solid black;
+          }
+        `}</style>
+      Data
+    </div>
+  );
+}
+
+export default Footer;
