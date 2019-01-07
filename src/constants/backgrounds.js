fc200e0a44a01a4fe77a36be2cd781be3ff0063e
Justin Gomez <programjustin@gmail.com>
2019-01-06 18:23:35 -0800

    export an object that houses backgrounds

---
+++ src/constants/backgrounds.js
@@ -0,0 +1,12 @@
+import AJ from '../../public/AirJordanBackground.jpg';
+import PH from '../../public/pharell.jpg';
+
+console.log(AJ);
+
+const AirJordan = AJ;
+const Pharell = PH;
+
+export default {
+  airJordan: AirJordan,
+  pharell: Pharell
+}
