930c583a7ff0fcebe1acbf35288cb488814c0383
Justin Gomez <programjustin@gmail.com>
2019-01-08 17:22:17 -0800

    change background image loading approach to make Header component more modular

--- src/components/HeroLanding1.jsx
+++ src/components/HeroLanding1.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import AirJordan from '../../public/AirJordanBackground.jpg';
 
 class HeroLanding1 extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      backgroundImage: AirJordan,
+      backgroundImage: '',
     };
   }
 
@@ -14,40 +13,54 @@ class HeroLanding1 extends React.Component {
   }
 
   setBackground(){
-    this.setState({backgroundImage: AirJordan});
+    // console.log('props is', this.props.airJordan);
+    if(this.props.airJordan){
+      this.setState({backgroundImage: this.props.airJordan});
+    } else {
+      return console.log('error')
+    }
   }
   render(){
-    return(
-      <div id="heroLanding1">
-        <style global jsx>{`
-              #heroLanding1 {
-                height: 525px;
-                display: flex;
-                flex-direction: row;
-                justify-content: flex-end;
-                align-items: center;
-                background-image: url(${this.state.backgroundImage});
-                background-size: cover;
-              }
-              #contentContainerHero1 {
-                margin-right: 60px;
-                color: white;
-              }
-              #airJordanButton {
-                width: 75px;
-                border-radius: 2px;
-                padding: 15px;
-                background-color: white;
-                margin-top: 20px;
-                color: black;
-              }
-          `}</style>
-          <div id="contentContainerHero1">
-            <h1>Air Jordan 1</h1>
-            <div id="airJordanButton">Shop All</div>
-          </div>
-      </div>
-    );
+    if(this.state.backgroundImage){
+      return(
+        <div id="heroLanding1">
+          <style global jsx>{`
+                #heroLanding1 {
+                  height: 525px;
+                  display: flex;
+                  flex-direction: row;
+                  justify-content: flex-end;
+                  align-items: flex-end;
+                  background-image: url(${this.state.backgroundImage});
+                  background-size: cover;
+                }
+                #contentContainerHero1 {
+                  margin-right: 100px;
+                  margin-bottom: 100px;
+                  color: white;
+                }
+                #airJordanButton {
+                  width: 75px;
+                  border-radius: 2px;
+                  padding: 15px;
+                  background-color: white;
+                  margin-top: 20px;
+                  color: black;
+                }
+            `}</style>
+            <div id="contentContainerHero1">
+              <h1>Air Jordan 1</h1>
+              <div id="airJordanButton">Shop All</div>
+            </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          ...loading
+        </div>
+      );
+    }
   }
 }
 
