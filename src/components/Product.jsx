2713d12c71c4838d558b75e88cc0bdc0f4351cad
Justin Gomez <programjustin@gmail.com>
2019-01-11 17:59:57 -0800

    add image and styling to Product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,30 @@
 import React from  'react';
+import mock from '../../public/mock.png';
 
 function Post(){
   return(
-    <div></div>
+    <div className="product">
+      <style jsx global>{`
+        .product {
+          width: 400px;
+          height: 371px;
+          border: 1px solid gray;
+          margin: 20px;
+        }
+        .imgContainer {
+          width: inherit;
+        }
+        .imgContainer img {
+          width: inherit;
+        }
+
+
+
+        `}</style>
+      <div className="imgContainer">
+        <img src={mock} />
+      </div>
+    </div>
   );
 }
 
