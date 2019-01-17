ab0d796e03c6829e9b08fdb1cda313806e222df5
Justin Gomez <programjustin@gmail.com>
2019-01-16 20:47:01 -0800

    import image

--- src/constants/backgrounds.js
+++ src/constants/backgrounds.js
@@ -1,10 +1,14 @@
 import AJ from '../../public/AirJordanBackground.jpg';
 import PH from '../../public/pharell.jpg';
+import AIRMAX97 from '../../public/airmax97sw.jpg';
 
 const AirJordan = AJ;
 const Pharell = PH;
+const airmax97sw = AIRMAX97;
 
 export default {
   airJordan: AirJordan,
-  pharell: Pharell
+  pharell: Pharell,
+  airmax97sw: airmax97sw
+
 }
